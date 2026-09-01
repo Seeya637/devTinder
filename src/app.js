0e718f7f9a1fdcbc75a924edada7c218d02cdc9a
Seeya637 <bagrawal72077@email.com>
2026-09-01 16:50:25 +0530

    multiple request handlers

--- src/app.js
+++ src/app.js
@@ -1,28 +1,24 @@
-//main core js file. start of file
-
 const express=require('express');
 
 const app=express();
 
-let user={ firstname:'Akshay', lastname:'saini'};
+app.get('/user',(req,res,next)=>{
+    console.log("handling the route user!");
+    next();
+    // res.send("response");
+},
+(req,res,next)=>{
+    console.log("handling the route user 2");
+    next();
+    // res.send("2 nd response");
+},
+(req,res,next)=>{
+    console.log("handling the route user 3");
+     res.send("final response");
+     next();
+}
+)
 
-app.get("/user",(req,res)=>{
-    res.send({firstname:"Akshay",lastname:"Saini"});
+app.listen('7777',()=>{
+        console.log('server is running on port number 7777');
 });
-
-app.post("/user",(req,res)=>{
-    res.send("data successfully saved to database!");
-});
-
-app.delete("/user",(req,res)=>{
-    res.send("data successfully deleted from database")
-});
-
-
-app.patch("/user",(req,res)=>{
-    res.send({})
-})
-app.listen(7777,()=>{
-    console.log("server is successfully listening on port 7777...");
-});
-
